2260347260aac17ebb4c949c022d27d5a1f19b44
jamescurneen <142897284+jamescurneen@users.noreply.github.com>
2023-12-12 15:26:31 +0000

    Update create_symlinks.sh

--- bin/create_symlinks.sh
+++ bin/create_symlinks.sh
@@ -1,3 +1,16 @@
+/*
+This Bash script, part of the oref0 system, performs a series of commands to create symbolic links for executable files specified within the oref0 package.
+
+The script executes the following steps:
+1. Gathers relevant executable file information from the package.json file using 'grep' and 'cut' commands.
+2. Filters out specific file types (js, sh, py) and excludes certain file names (bt-pan, l, wifi) using 'grep' and 'sed'.
+3. Processes each identified file by removing the './bin/' prefix using 'sed'.
+4. Loops through the list of identified files and creates symbolic links for each file if a link with the same name doesn’t exist already.
+
+The script checks for file suffixes and avoids creating symbolic links for files that already possess suffixes. Additionally, it ensures not to recreate existing links to maintain integrity within the oref0 system, enhancing file accessibility and execution across the system.
+*/
+
+
 #!/usr/bin/env bash
 
 grep bin ../package.json |\
